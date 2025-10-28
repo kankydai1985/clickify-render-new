9cba3a360d6ce7bba19532e25e95b1f8f21cf527
kankydai1985 <kankydai85@gmail.com>
2025-10-28 17:21:02 +0500

    Update chromium import from 'chromium-min' to 'chromium'

--- api/preview.js
+++ api/preview.js
@@ -1,6 +1,6 @@
 // api/preview.js
 import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium-min';
+import chromium from '@sparticuz/chromium';
 import fetch from 'node-fetch';
 import { generateLayout } from './layout/ai-layout-generator.js';
 
